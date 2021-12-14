a10b5e0b77705bcc318317ef6a37e454987fb295
Ziddi-Mian <ahmadyounas893@gmail.com>
2021-12-14 16:56:41 +0500

    negative

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ console.log(a);
 
 
 // js program to print all negative Element in array
+ 
+var myArray = [4, -5, 0, 2, -67, 8, 10, -34 ];
+
+function getNegativeNumbers(array) {
+
+    var negatives = [];
+
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] < 0) {
+            negatives.push(array[i]);
+        }
+    }
+    return negatives;
+
+}
+console.log(getNegativeNumbers(myArray));
