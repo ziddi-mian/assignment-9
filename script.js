8126715d9b51e434f8263211854cdd311ce9a70d
Ziddi-Mian <ahmadyounas893@gmail.com>
2021-12-17 15:02:04 +0500

    bsb
    jaaava

--- script.js
+++ script.js
@@ -85,7 +85,23 @@ console.log("PositiveCount :: " + posCount);
 console.log("NegativeCount:: " + negativeCount);
 
 
+//Write a js program to insert an element in an array.
 
+
+function insertElement() {
+    let array = [1, 2, 3, 4, 5];
+
+   
+    let index = 3;
+
+    
+    let element = 8;
+  
+    array.splice(index, 0, element);
+    console.log(array);
+}
+
+insertElement();
 
 
 
