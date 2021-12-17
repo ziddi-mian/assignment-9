936ebba9eeed5f710291d844bda7906c59142bd0
Ziddi-Mian <ahmadyounas893@gmail.com>
2021-12-17 15:03:19 +0500

    jaavav

--- script.js
+++ script.js
@@ -104,6 +104,18 @@ function insertElement() {
 insertElement();
 
 
+// Write a js program to copy all elements from an array to another array.
+
+var first = [1, 2, 3];
+var second = [4, 5];
+ 
+for (var i of second) {
+    first.push(i);
+}
+ 
+console.log(first);
+
+
 
 
 
